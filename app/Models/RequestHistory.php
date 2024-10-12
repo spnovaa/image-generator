46b3de84c6dc2ae8941b2afe33b5774a2d7413a6
SPNOVA\playe <mm.rahmati94@gmail.com>
2024-10-12 12:21:30 +0330

    REQUEST: renamed to prevent confusion between HTTP Request and Request model.

--- app/Models/RequestHistory.php
+++ app/Models/RequestHistory.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Request extends Model
+class RequestHistory extends Model
 {
     protected $table = 'Requests';
     protected $primaryKey = 'R_Id';

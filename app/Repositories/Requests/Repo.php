abcb5f93bab62d2cc7b586e591045f554f2babf2
SPNOVA\playe <mm.rahmati94@gmail.com>
2024-10-14 01:52:41 +0330

    REQUEST: Repo implemented.

---
+++ app/Repositories/Requests/Repo.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Repositories\Requests;
+
+use App\Enums\Requests\Status;
+use App\Exceptions\GeneralDatabaseException;
+use App\Models\RequestHistory;
+use Illuminate\Database\Eloquent\Collection;
+use InvalidArgumentException;
+use Throwable;
+
+class Repo
+{
+    /**
+     * @param RequestHistory $request
+     * @return RequestHistory
+     * @throws GeneralDatabaseException
+     */
+    public function create(RequestHistory $history): RequestHistory
+    {
+        try {
+            $history->save();
+            return $history;
+        } catch (Throwable) {
+            throw new GeneralDatabaseException();
+        }
+    }
+
+
+    /**
+     * @param RequestHistory $history
+     * @return RequestHistory
+     * @throws GeneralDatabaseException
+     */
+    public function update(RequestHistory $history): RequestHistory
+    {
+        try {
+            if ($history->getOriginal('R_Id'))
+                $history->save();
+            else
+                throw new InvalidArgumentException('SERVER ERROR!');
+
+            return $history;
+        } catch (InvalidArgumentException $exception) {
+            throw $exception;
+        } catch (Throwable) {
+            throw new GeneralDatabaseException();
+        }
+    }
+
+    /**
+     * @param int $id
+     * @return RequestHistory|null
+     * @throws GeneralDatabaseException
+     */
+    public function show(int $id): ?RequestHistory
+    {
+        try {
+            return RequestHistory::find($id);
+        } catch (Throwable) {
+            throw new GeneralDatabaseException();
+        }
+    }
+
+    /**
+     * @return Collection
+     * @throws GeneralDatabaseException
+     */
+    public function index(): Collection
+    {
+        try {
+            return RequestHistory::all();
+        } catch (Throwable) {
+            throw new GeneralDatabaseException();
+        }
+    }
+
+    /**
+     * @return Collection
+     * @throws GeneralDatabaseException
+     */
+    public function getReadyRecords(): Collection
+    {
+        try {
+        return RequestHistory::where('R_Status', Status::PENDING)->get();
+        } catch (Throwable) {
+            throw new GeneralDatabaseException();
+        }
+    }
+}
